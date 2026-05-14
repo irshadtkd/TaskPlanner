554f8f7e0fbad4f97d7527cf143eb27fb88a2609
irshadtkd <irshadtkd@gmail.com>
2026-05-14 14:17:43 +0530

    initial commit

---
+++ TaskPlanner/TaskPlanner/TaskPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskPlannerApp.swift
+//  TaskPlanner
+//
+//  Created by Irshadali M T on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
